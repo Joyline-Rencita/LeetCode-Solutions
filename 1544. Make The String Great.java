d3045638635157240ba60fa5c4c88d61a0903127
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2024-04-07 12:01:52 +0530

    Create 1544. Make The String Great.java

---
+++ 1544. Make The String Great.java
@@ -0,0 +1,31 @@
+Given a string s of lower and upper case English letters. A good string is a string which doesn't have two adjacent characters s[i] and s[i + 1] where:
+0 <= i <= s.length - 2
+s[i] is a lower-case letter and s[i + 1] is the same letter but in upper-case or vice-versa.
+To make the string good, you can choose two adjacent characters that make the string bad and remove them. You can keep doing this until the string becomes good.
+Return the string after making it good. The answer is guaranteed to be unique under the given constraints.
+Example 1:
+
+Input: s = "leEeetcode"
+Output: "leetcode"
+Explanation: In the first step, either you choose i = 1 or i = 2, both will result "leEeetcode" to be reduced to "leetcode".
+
+import java.util.Stack;
+
+class Solution {
+    public int maxDepth(String s) {
+        int depth = 0;
+        int count = 0;
+        Stack<Character> brackets = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                brackets.push(')');
+                count++;
+            } else if (s.charAt(i) == ')') {
+                depth = Math.max(depth, count);
+                count--;
+                brackets.pop();
+            }
+        }
+        return depth;
+    }
+}
