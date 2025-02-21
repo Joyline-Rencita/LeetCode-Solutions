2bd336a3357fede5a56018f8e75527930b971866
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-02-21 12:56:10 +0400

    Update 0182_Duplicate emails.sql

--- 0182_Duplicate emails.sql
+++ 0182_Duplicate emails.sql
@@ -1,5 +1,14 @@
+My Solution :
+    
 SELECT DISTINCT p1.email
 FROM
     person AS p1,
     person AS p2
 WHERE p1.id != p2.id AND p1.email = p2.email;
+
+
+Better Solution :
+
+SELECT email from Person
+group by email
+having count(email) > 1;
