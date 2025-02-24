1a57ecdd0d3599be5c39a4551814cd2826ba77de
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-02-24 16:57:38 +0400

    Create 1978_Employees Manager who left the country.sql

---
+++ 1978_Employees Manager who left the country.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+SELECT e1.employee_id
+FROM
+    Employees AS e1
+    LEFT JOIN Employees AS e2 ON e1.manager_id = e2.employee_id
+WHERE e1.salary < 30000 AND e1.manager_id IS NOT NULL AND e2.employee_id IS NULL
+ORDER BY 1;
+
+
+# Write your MySQL query statement below
+SELECT employee_id
+FROM Employees
+WHERE salary < 30000 AND manager_id NOT IN (SELECT employee_id FROM Employees)
+ORDER BY 1;
