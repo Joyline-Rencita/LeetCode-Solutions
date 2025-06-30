f15c696a962d76ddccf86cdf6751532603ba24a4
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-06-30 20:22:06 +0530

    Update 0152_Maximum product subarray.java

--- 0152_Maximum product subarray.java
+++ 0152_Maximum product subarray.java
@@ -10,3 +10,31 @@ class Solution {
         return ans;
     }
 }
+
+
+********************************************************************
+
+class Solution {
+
+    public int maxProduct(int[] nums) {
+        int max = nums[0];
+        int currentMax = nums[0];
+        int currentMin = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            int num = nums[i];
+
+            if (num < 0) {
+                int temp = currentMax;
+                currentMax = currentMin;
+                currentMin = temp;
+            }
+
+            currentMax = Math.max(num, currentMax * num);
+            currentMin = Math.min(num, currentMin * num);
+            max = Math.max(max, currentMax);
+        }
+
+        return max;
+    }
+}
