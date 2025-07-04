6d70196cd3641433522a8ad879b0556bdd3bc3cd
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-07-04 10:46:32 +0530

    Update 0409_Longest_Palindrome.java

--- 0409_Longest_Palindrome.java
+++ 0409_Longest_Palindrome.java
@@ -1,26 +1,20 @@
 class Solution {
     public int longestPalindrome(String s) {
-        // Create an array to count occurrences of each character.
-        // The ASCII value of a character will be used as the index.
+        // Create an array to count occurrences of each character. The ASCII value of a character will be used as the index.
         int[] charCounts = new int[128];
         for (int i = 0; i < s.length(); i++) {
-            // Count each character's occurrences.
-            charCounts[s.charAt(i)]++;
+            charCounts[s.charAt(i)]++;                        // Count each character's occurrences.
         }
-
         int lengthOfLongestPalindrome = 0;
         for (int count : charCounts) {
-            // Add the largest even number below or equal to the current character count.
-            // This is equivalent to count - (count % 2).
+            // Add the largest even number below or equal to the current character count. This is equivalent to count - (count % 2).
             lengthOfLongestPalindrome += count - (count & 1);
 
-            // If the current palindrome length is even and the count is odd,
-            // we can add one more character to the center of the palindrome.
+            // If the current palindrome length is even and the count is odd, we can add one more character to the center of the palindrome.
             if (lengthOfLongestPalindrome % 2 == 0 && count % 2 == 1) {
                 lengthOfLongestPalindrome++;
             }
         }
-        // Return the length of the longest possible palindrome.
-        return lengthOfLongestPalindrome;
+        return lengthOfLongestPalindrome;                   // Return the length of the longest possible palindrome.
     }
 }
