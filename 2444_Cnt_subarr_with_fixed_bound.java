476c05fc275a5b042be4f3e0dd81fd7ec40cdb17
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-07-08 22:47:35 +0530

    Update and rename 2444_cnt_subarr_with_fixed_bound.java to 2444_Cnt_subarr_with_fixed_bound.java

--- 2444_Cnt_subarr_with_fixed_bound.java
+++ 2444_Cnt_subarr_with_fixed_bound.java
@@ -5,7 +5,7 @@ Example 1:
 Input: nums = [1,3,5,2,7,5], minK = 1, maxK = 5          Output: 2             Explanation: The fixed-bound subarrays are [1,3,5] and [1,3,5,2].
 
 Example 2:
-Input: nums = [1,1,1,1], minK = 1, maxK = 1              Output: 10             Explanation: Every subarray of nums is a fixed-bound subarray. There are 10 possible subarrays.
+Input: nums = [1,1,1,1], minK = 1, maxK = 1              Output: 10            Explanation: Every subarray of nums is a fixed-bound subarray. There are 10 possible subarrays.
 
 Constraints:
 2 <= nums.length <= 105 ,       1 <= nums[i], minK, maxK <= 106
